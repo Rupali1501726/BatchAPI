672ca5d97713ef6bf5be4faebe3e0e0065277ae1
Rupali Karawade <rupal1501726@mastek.com>
2023-06-01 05:51:56 +0530

    unit test project created

---
+++ BatchProject.Test/BatchControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BatchAPI.Entities;
+using BatchAPI.Services;
+using FakeItEasy;
+using Microsoft.AspNetCore.Http;
+
+namespace BatchProject.Test
+{
+    public class BatchControllerTest
+    {
+        
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+        [Test]
+        public void TestAddBatch()
+        {
+            IBatchServices batchServices = A.Fake<IBatchServices>();
+            AddBatchResponse objAddBatchResponse = A.Fake<AddBatchResponse>();
+            A.CallTo(() => batchServices.PostBatch(A<BatchRequest>._, A<IFormFile>._));
+            Assert.IsNotNull(objAddBatchResponse);
+        }
+        [Test]
+        public void TestGetBatch()
+        {
+            IBatchServices batchServices = A.Fake<IBatchServices>();
+            Batch objbatch = A.Fake<Batch>();
+            A.CallTo(() => batchServices.GetBatch(A<Guid>._));
+            Assert.IsNotNull (objbatch);
+        }
+    }
+}
